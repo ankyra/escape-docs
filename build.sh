f5f4dab0456e31e0563d39ce80105719f855b46b
Bart Spaans <bart.spaans@gmail.com>
2017-07-27 03:42:27 +0100

    Command line reference for escape-client

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash -e
 
-rm -rf content/docs/escape-client/
-mkdir -p content/docs/
-cp -r deps/_/escape-client/docs/cmd content/docs/escape-client
+rm -rf content/docs
+mkdir -p content/
+cp -r deps/_/escape-client/docs/cmd content/docs
+
+for x in content/docs/*.md ; do 
+    content=$(cat $x| sed 's|(\(.*\)\.md)|(../\1/)|g')
+    title=$(head -1 < $x | sed 's/#//g')
+    slug=$(basename $x)
+    read -d '' header <<EOF || true
++++
+summary = ""
+title = "$title"
+slug = "${slug/.md}"
+type = "docs"
++++
+
+EOF
+    echo -e "$header\n$content" > $x
+done
+
 hugulp build
