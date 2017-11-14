63ea9dbe8df97586a44941ee2b17ae3d9d869d54
Bart Spaans <bart.spaans@gmail.com>
2017-11-14 15:14:01 +0000

    Copy generated docs from escape package

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ DEPS="deps/heist/escape-integration-tests/deps"
 rm -rf content/docs
 mkdir -p content/
 cp -r ${DEPS}/_/escape/docs/cmd content/docs
+cp ${DEPS}/_/escape/docs/generated/*.md content/docs
 cp ${DEPS}/_/escape/docs/*.md content/docs/
 cp ${DEPS}/_/escape/vendor/github.com/ankyra/escape-core/docs/generated/*.md content/docs/
 cp ${DEPS}/_/escape-inventory/docs/*.md content/docs/
